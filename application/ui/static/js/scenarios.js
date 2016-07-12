8784f73e5c8b2543fd7aefbcda74101c3936a7f8
David Gwartney <davidg@boundary.com>
2016-07-12 07:52:12 -0700

    Remove alerts ; Better descriptions of scenarios ; UI clean up

--- application/ui/static/js/scenarios.js
+++ application/ui/static/js/scenarios.js
@@ -1,60 +1,50 @@
 $('#action_1_1').click(function () {
-    alert('ajax');
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/v1/api/scenario/1?action_id=1",
         success: function (data) {
-            alert(data);
         }
-    });
+    })
 });
 
 
 $('#action_2_1').click(function () {
-    alert('ajax');
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/v1/api/scenario/2?action_id=1",
         success: function (data) {
-            alert(data);
         }
     });
 });
 
 $('#action_2_2').click(function () {
-    alert('ajax');
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/v1/api/scenario/2?action_id=1",
         success: function (data) {
-            alert(data);
         }
     });
 });
 
 $('#action_3_1').click(function () {
-    alert('ajax');
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/v1/api/scenario/3?action_id=1",
         success: function (data) {
-            alert(data);
         }
     });
 });
 
 $('#action_3_2').click(function () {
-    alert('ajax');
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/v1/api/scenario/3?action_id=1",
         success: function (data) {
-            alert(data);
         }
     });
 });
