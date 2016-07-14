49c8e19900ad694abbda8d6e5e542c93f378ec8d
David Gwartney <davidg@boundary.com>
2016-07-14 08:30:17 -0700

    Change host to be referenced by name

--- application/api/scenarios/cpu_load_scenario.py
+++ application/api/scenarios/cpu_load_scenario.py
@@ -7,7 +7,7 @@ class CPULoadOnWebServer(Scenario):
     def __init__(self, id):
         super(CPULoadOnWebServer, self).__init__(id)
 
-        self._actions[1] = CPULoadAction(1, "52.52.34.20")
+        self._actions[1] = CPULoadAction(1, "web-1")
 
     def execute(self, action_id):
         action = self._actions[int(action_id)]
