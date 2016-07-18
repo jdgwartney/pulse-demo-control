7e51bd4d88bea56508cb6c0616128e25428e89a9
David Gwartney <davidg@boundary.com>
2016-07-18 09:56:39 -0700

    updated text and graphics

--- application/ui/static/js/devops.js
+++ application/ui/static/js/devops.js
@@ -12,37 +12,25 @@ function launchApplication(l_url, l_windowName) {
     if (typeof launchApplication.winRefs == 'undefined') {
         launchApplication.winRefs = {};
     }
-    if (typeof launchApplication.winRefs[l_windowName] == 'undefined' || launchApplication.winRefs[l_windowName].closed) {
-        var l_width = screen.availWidth;
+    if (typeof launchApplication.winRefs[l_windowName] == 'undefined' ||
+        launchApplication.winRefs[l_windowName].closed) {
+        var l_width = screen.availWidth/2;
         var l_height = screen.availHeight;
+        var l_left = screen.availWidth/2;
         var l_params = 'status=1' +
             ',resizable=1' +
             ',scrollbars=1' +
             ',width=' + l_width +
             ',height=' + l_height +
-            ',left=0' +
+            ',left=' + l_left +
             ',top=0';
-        l_params = 'status=1' + ',resizable=1' + ',scrollbars=1';
 
 
         launchApplication.winRefs[l_windowName] = window.open(l_url, l_windowName, l_params);
-        // launchApplication.winRefs[l_windowName] = window.open(l_url, l_windowName);
-        launchApplication.winRefs[l_windowName].moveTo(0, 0);
+        launchApplication.winRefs[l_windowName].moveTo(l_left, 0);
         launchApplication.winRefs[l_windowName].resizeTo(l_width, l_height);
     } else {
         launchApplication.winRefs[l_windowName].focus()
-    }
-}
-
-function open_pulse_window(url, item) {
-    var pulse_window = localStorage.getItem(item);
-    if (pulse_window) {
-        console.log("changing focus to: " + url);
-        pulse_window.focus();
-    } else {
-        pulse_window = window.open(url, item);
-        localStorage.setItem(item, pulse_window);
-        pulse_window.focus();
     }
 }
 
