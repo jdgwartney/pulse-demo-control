823e9ee5744e4c7ebcabc6bcdcd2300803374957
David Gwartney <davidg@boundary.com>
2016-07-18 08:50:56 -0700

    Add new text from Seth

--- application/ui/static/js/devops.js
+++ application/ui/static/js/devops.js
@@ -8,6 +8,32 @@ $('#action_1_1').click(function () {
     })
 });
 
+function launchApplication(l_url, l_windowName) {
+    if (typeof launchApplication.winRefs == 'undefined') {
+        launchApplication.winRefs = {};
+    }
+    if (typeof launchApplication.winRefs[l_windowName] == 'undefined' || launchApplication.winRefs[l_windowName].closed) {
+        var l_width = screen.availWidth;
+        var l_height = screen.availHeight;
+        var l_params = 'status=1' +
+            ',resizable=1' +
+            ',scrollbars=1' +
+            ',width=' + l_width +
+            ',height=' + l_height +
+            ',left=0' +
+            ',top=0';
+        l_params = 'status=1' + ',resizable=1' + ',scrollbars=1';
+
+
+        launchApplication.winRefs[l_windowName] = window.open(l_url, l_windowName, l_params);
+        // launchApplication.winRefs[l_windowName] = window.open(l_url, l_windowName);
+        launchApplication.winRefs[l_windowName].moveTo(0, 0);
+        launchApplication.winRefs[l_windowName].resizeTo(l_width, l_height);
+    } else {
+        launchApplication.winRefs[l_windowName].focus()
+    }
+}
+
 function open_pulse_window(url, item) {
     var pulse_window = localStorage.getItem(item);
     if (pulse_window) {
@@ -20,21 +46,20 @@ function open_pulse_window(url, item) {
     }
 }
 
-$("#step_3_1_action").click(function() {
+$("#step_3_1_action").click(function () {
     var progression = 0,
-    progress = setInterval(function() 
-    {
-        $('#code-fix-progress').css("width", progression + "%");
-        $('#code-fix-progress').attr("aria-valuenow", progression + "%");
-        $('#code-fix-progress').text(progression + '%');
+        progress = setInterval(function () {
+            $('#code-fix-progress').css("width", progression + "%");
+            $('#code-fix-progress').attr("aria-valuenow", progression + "%");
+            $('#code-fix-progress').text(progression + '%');
 
-        if(progression == 100) {
-            clearInterval(progress);
-            $('#code-fix-progress').text('Done!');
-        } else
-            progression += 10;
+            if (progression == 100) {
+                clearInterval(progress);
+                $('#code-fix-progress').text('Done!');
+            } else
+                progression += 10;
 
-    }, 1000);
+        }, 1000);
 });
 
 
