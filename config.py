bedc5b6803ccd9d3005fb5e1c15706209bd75bd7
David Gwartney <davidg@boundary.com>
2016-07-31 19:45:12 -0700

    Add configuration

---
+++ config.py
@@ -0,0 +1,55 @@
+#
+# Copyright 2016 BMC Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+basedir = os.path.abspath(os.path.dirname(__FILE__))
+
+
+class Config(object):
+
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' \
+                                                                    + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
+
+
+
+
